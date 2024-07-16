6ee745621014f16d605f561960de4424a5fa3a66
ArielCann <acann@student.touro.edu>
2024-07-15 20:48:48 -0400

    revised shuffle.py to shrink number of distinct names to a bit above 2000, and to sort by username to make it easier to analyze personal history. Note: shuffle.py relies on having jikeliCorpus.xlsx in the same directory.

--- shuffle/order_utils.py
+++ shuffle/order_utils.py
@@ -6,6 +6,9 @@ import random
 
 
 def date_range(past: datetime.datetime, numdates: int, years: int) -> list[datetime.datetime]:
+    """:param past: the beginning of the time period
+    :param numdates: the number of dates in the time period
+    :param years: the number of years in the time period"""
     dates = list()
     for i in range(0, numdates):
         dates.append(past + datetime.timedelta(days=random.randint(0, years * 365), hours=random.randint(0, 24),
@@ -13,8 +16,13 @@ def date_range(past: datetime.datetime, numdates: int, years: int) -> list[datet
     return dates
 
 
-def clustered_random_dates(past: datetime.datetime, cluster_size: int, num_cluster: int, years: int, remainder=0) -> \
-list[datetime.datetime]:
+def clustered_random_dates(past: datetime.datetime, cluster_size: int, num_cluster: int, years=1, remainder=0) -> \
+        list[datetime.datetime]:
+    """:param past: the beginning of the time period
+    :param num_cluster: the number of clusters in the time period
+    :param cluster_size: the size of each cluster of date times
+    :param years: the number of years in the time period
+    :param remainder: the number of non-clustered date times"""
     dates = list()
     for cluster in range(0, num_cluster):
         date = past + datetime.timedelta(days=random.randint(0, years * 365), hours=random.randint(0, 24),
@@ -26,17 +34,23 @@ list[datetime.datetime]:
     return dates
 
 
-def followers(names: list[str]) -> dict[str, list[str]]:
-    follower_list = dict()
-    for i in range(0, len(names)):
-        follower_list[names[i]] = (numpy.random.choice(a=names, size=random.randint(3, 8), replace=False))
-    return follower_list
+def followers(follower_names: list[str], leader_names: list[str], connectivity=5) -> dict[str, list[str]]:
+    """:param follower_names: the names of the followers
+    :param leader_names: the usernames that can be followed
+    :param connectivity: the number of people each follower follows"""
+    following = dict()
+    for i in range(0, len(follower_names)):
+        following[follower_names[i]] = numpy.random.choice(a=leader_names, size=connectivity, replace=False)
+    return following
 
 
 def replace_names(names: list[str], target: list[str]) -> list[str]:
+    """:param names: the names being written in
+    :param target: the names being written over"""
+    newList = target
     for i in range(len(target)):
-        target[i] = names[random.randint(0, len(names) - 1)]
-    return target
+        newList[i] = names[random.randint(0, len(names) - 1)]
+    return newList
 
 
 def weight_bag(cluster_size: int, cluster_num, arr: list):
