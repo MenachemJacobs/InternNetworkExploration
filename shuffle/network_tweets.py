8a344ed92f04e9d0f1254b28814ca15776af7949
ArielCann <20acann@gmail.com>
2024-07-18 15:23:28 -0400

    xml

--- shuffle/network_tweets.py
+++ shuffle/network_tweets.py
@@ -10,51 +10,55 @@ jikeliCorpus = pandas.read_excel('jikeliCorpus.xlsx', header=1)
 anti_tweets = store_indices(jikeliCorpus['Biased'], 1)
 pro_tweets = store_indices(jikeliCorpus['Biased'], 0)
 mngr = NetworkManager()
+mngr.set_overt_list(default_overt_list)
+mngr.set_covert_list(default_covert_list)
+mngr.set_pro_list(default_pro_list)
 mngr.generate_overt_network()
 mngr.generate_covert_network()
-for user in default_covert_list:
+mngr.generate_pro_network()
+for user in mngr.covert_list:
     user.messages = list()
-for user in default_overt_list:
+for user in mngr.overt_list:
     user.messages = list()
 covert_tweets = list()
-for i in pro_tweets[:4000]:
+for i in pro_tweets[:3000]:
     covert_tweets.append(replace_words(clean(jikeliCorpus['Text'][i]), hotwords.hot_words + hotwords.hot_phrases, 0.4))
 for tweet in covert_tweets[:2000]:
-    agent = numpy.random.choice(range(0, len(default_covert_list)), size=1)
+    agent = numpy.random.choice(range(0, len(mngr.covert_list)), size=1)
     rand_date = new_dates[random.choice(range(0, len(new_dates)))]
     msg = Message()
     msg.date = rand_date
     msg.text = ' '.join(tweet)
     msg.score = random.uniform(0.2,0.5)
-    default_covert_list[agent[0]].messages.append(msg)
+    mngr.covert_list[agent[0]].messages.append(msg)
 for tweet in covert_tweets[2001:4000]:
-    agent = numpy.random.choice(range(0, len(default_overt_list)), size=1)
+    agent = numpy.random.choice(range(0, len(mngr.overt_list)), size=1)
     rand_date = new_dates[random.choice(range(0, len(new_dates)))]
     msg = Message()
     msg.date = rand_date
     msg.text = ' '.join(tweet)
     msg.score = random.uniform(0.2,0.5)
-    default_overt_list[agent[0]].messages.append(msg)
+    mngr.overt_list[agent[0]].messages.append(msg)
 for index in anti_tweets[:2000]:
     tweet = jikeliCorpus['Text'][index]
-    agent = numpy.random.choice(range(0, len(default_overt_list)), size=1)
+    agent = numpy.random.choice(range(0, len(mngr.overt_list)), size=1)
     rand_date = new_dates[random.choice(range(0, len(new_dates)))]
     msg = Message()
     msg.date = rand_date
     msg.text = tweet
     msg.score = random.uniform(0.75,1.00)
-    default_overt_list[agent[0]].messages.append(msg)
+    mngr.overt_list[agent[0]].messages.append(msg)
 users = list()
 message_list = list()
 antisemitic = list()
 friends = list()
 scores = list()
-for user in default_covert_list:
+for user in mngr.covert_list:
     antisemitic.append(user.antisemite)
     users.append(user.name)
     message_list.append([(msg.text, msg.score,msg.date) for msg in user.messages])
     friends.append(user.subscriptions)
-for user in default_overt_list:
+for user in mngr.overt_list:
     user.antisemite = True
     antisemitic.append(user.antisemite)
     users.append(user.name)
