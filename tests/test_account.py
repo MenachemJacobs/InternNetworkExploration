4902eb8e8bbe34105f948f38c7410c0688326e3c
ArielCann <20acann@gmail.com>
2024-07-21 18:22:18 -0400

    refactored tests to use setup method and added testing for hot word compilation.

---
+++ tests/test_account.py
@@ -0,0 +1,35 @@
+import unittest
+from Components.Account import Account
+from Components.Message import Message
+from ContextGeneration.GenerateNameNetworks import *
+from AdversaryRevulsion import *
+from shuffle.create_accounts import accountData
+import shuffle.utils
+
+
+class TestAccountMethods(unittest.TestCase):
+    def setUp(self):
+        self.accounts = list()
+        for index in range(len(accountData['Username'])):
+            msg_data = accountData['Messages'][index]
+            msg_list = [(shuffle.utils.tuple_to_message(data)) for data in msg_data]
+            account = Account.Account(accountData['Username'][index], msg_list,
+                                      accountData['Subscriptions'][index], accountData['Antisemitic'][index])
+            self.accounts.append(account)
+        self.listener = CovertLister()
+        self.listener.all_accounts = self.accounts
+
+    def test_uncover(self):
+        self.assertTrue(self.listener.uncover_overt(), "Not detecting overt accounts.")
+        self.assertTrue(self.listener.uncover_covert(self.accounts), "Returning empty list of covert accounts.")
+
+    def test_hotlist(self):
+        suspects = [account[0] for account in self.listener.uncover_covert(all_accounts=self.accounts)]
+        self.listener.uncover_overt()
+        self.listener.compile_hot_lists(suspects)
+        self.assertTrue(self.listener.hot_phrases, "no hot phrases identified")
+        self.assertTrue(self.listener.hot_words, "no hot words identified")
+
+
+if __name__ == '__main__':
+    unittest.main()
