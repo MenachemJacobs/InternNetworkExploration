fbedc5de63540c11af7041d3ddef1f23b3c08669
ArielCann <20acann@gmail.com>
2024-07-21 17:59:00 -0400

    changed App.py and create_accounts.py to increase frequency of ngram substitution. renamed shuffle.py to move.py so as not to be confused with module name.

--- App.py
+++ App.py
@@ -1,10 +1,15 @@
 import csv
+import datetime
 
+import pandas as pd
+
+import shuffle.utils
+from shuffle.create_accounts import accountData
 from AdversaryRevulsion import CovertLister
 from Components.Account import Account
 from Components.Message import Message
 
-filename = 'shuffle/newData.csv'
+filename = 'shuffle/accounts.csv'
 
 Accounts: list["Account"] = []
 Covert_accounts: list["Account"] = []
@@ -23,40 +28,21 @@ def read_dataset(file):
     return data
 
 
-dataset = read_dataset(filename)
-
-for row in dataset[1:]:
-    timestamp = row[0]
-    biased = row[1] == "1"
-    username = row[2]
-    text = row[3]
-    users_followed = row[4]
-
-    message = Message(username, text)
-    account_found = False
-
-    for i in range(len(Accounts)):
-        if Accounts[i].name == username:
-            Accounts[i].messages.append(message)
-            Accounts[i].isAntisemite = Accounts[i].isAntisemite or biased
-            account_found = True
-            break
-
-    if not account_found:
-        new_account = Account(username, [message], [], biased)
-        new_account.subscriptions = row[4]
-        Accounts.append(new_account)
+dataset = accountData
+accounts = list()
+for row in range(len(dataset['Username'])):
+    messages = [shuffle.utils.tuple_to_message(msg) for msg in dataset['Messages'][row]]
+    antisemitic = dataset['Antisemitic'][row]
+    username = dataset['Username'][row]
+    subscriptions = dataset['Subscriptions'][row]
+    accounts.append(Account(name=username, messages=messages, initial_subscriptions=subscriptions,antisemite=antisemitic))
 
 myFinder = CovertLister()
-Covert_accounts = myFinder.uncover_covert(Accounts)
-# print(myFinder.hot_words[:10])
-# print(myFinder.hot_phrases[:10])
-# print(Covert_accounts[:10])
-for account in myFinder.overt_accounts:
-    friend_list = []
-
-    for i in range(len(Accounts)):
-        if Accounts[i].name in account.subscriptions:
-            friend_list.append(Accounts[i])
-
-    print(account, [(friend.name, friend.isAntisemite) for friend in friend_list])
+result = myFinder.uncover_covert(accounts)
+Covert_accounts = list()
+for part in result:
+    Covert_accounts.append(part[0])
+myFinder.compile_hot_lists(Covert_accounts)
+print(myFinder.hot_words[:10])
+print(myFinder.hot_phrases[:10])
+print(Covert_accounts[:10])
