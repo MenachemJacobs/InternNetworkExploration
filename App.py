2cadf733e81e93db80d5629555f206c8cc242924
Menolo-Homobovanez <118155256+MenachemJacobs@users.noreply.github.com>
2024-07-30 13:35:33 -0400

    Refactored load instruct to utils
    caught bugs in how account feature set is compiled

--- App.py
+++ App.py
@@ -3,35 +3,13 @@ import datetime
 from pandas import read_csv
 
 from ContextGeneration import Diagnostics
-from shuffle.utils import list_to_msg, parse_list_ints
+
 
 from AdversaryRevulsion import CovertLister
 from Components.Account import Account
+from shuffle import utils
 
-messageData = read_csv('shuffle/messages.csv')
-accountData = read_csv('shuffle/accounts.csv', converters={'Messages': parse_list_ints})
-
-# Create a dictionary for quick message lookup
-messageLookup = {}
-
-for _, row in messageData.iterrows():
-    msg_list = [datetime.datetime.strptime(row['Date'], "%d-%b-%Y (%H:%M:%S.%f)"),
-                row['Text'], row['Score'], row['Username']]
-    msg = list_to_msg(msg_list)
-    msg.ID = row['ID']
-    msg.replying_to = row['Replying_To']
-    messageLookup[int(row['ID'])] = msg
-
-# Create accounts
-accounts = set()
-
-for _, row in accountData.iterrows():
-    messages = {messageLookup[index] for index in row['Messages']}
-    antisemitic = row['Antisemitic']
-    username = row['Username']
-    subscriptions = row['Subscriptions']
-    accounts.add(Account(name=username, messages=messages, initial_subscriptions=subscriptions,
-                         antisemite=antisemitic))
+accounts = utils.load_accounts()
 
 myFinder = CovertLister()
 myFinder.classify(accounts)
@@ -39,11 +17,11 @@ myFinder.classify(accounts)
 gold = read_csv('shuffle/covert.csv')
 covert_gold = set(gold['Username'])
 
-for feature in myFinder.feature_set:
-    print(feature[:10])
-
 returned_accounts: set = {account[0].name for account in myFinder.covert_accounts[:10]}
 print(returned_accounts)
+
+for feature in myFinder.feature_set:
+    print(feature[:10])
 
 # difference_accounts = returned_accounts - covert_gold
 #
