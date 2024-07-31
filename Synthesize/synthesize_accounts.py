6725ba095af6a7ea1dd43d87e9a371b49b1e5197
Menolo-Homobovanez <118155256+MenachemJacobs@users.noreply.github.com>
2024-07-31 11:34:41 -0400

    your code is twice as long
    because you do everything twice

--- Synthesize/synthesize_accounts.py
+++ Synthesize/synthesize_accounts.py
@@ -1,54 +1,43 @@
 import pandas as pd
-from sdv.metadata import MultiTableMetadata, SingleTableMetadata
+from sdv.metadata import SingleTableMetadata
 from sdv.single_table import TVAESynthesizer
 
 from shuffle import utils
-from shuffle.utils import parse_list_ints
 
-anti_data = SingleTableMetadata()
-pro_data = SingleTableMetadata()
-accounts = utils.load_accounts()
-anti_df = pd.DataFrame(columns=['Avg_Score', 'Age_Score', 'Density_Score', 'Positivity', 'Antisemitic'], dtype=float)
-pro_df = pd.DataFrame(columns=['Avg_Score', 'Age_Score', 'Density_Score', 'Positivity', 'Antisemitic'], dtype=float)
-accounts = list(accounts)
-for index in range(len(accounts)):
-    acc = accounts[index]
+anti_data, pro_data = SingleTableMetadata(), SingleTableMetadata()
+anti_df = pd.DataFrame(columns=['Avg_Score', 'Age_Score', 'Density_Score', 'Positivity', 'Antisemitic'],
+                       dtype=float)
+pro_df = pd.DataFrame(columns=['Avg_Score', 'Age_Score', 'Density_Score', 'Positivity', 'Antisemitic'],
+                      dtype=float)
+
+accounts = list(utils.load_accounts())
+
+for index, acc in enumerate(accounts):
     acc.set_feature_scores()
-    if acc.isAntisemite:
-        anti_df.loc[index, 'Avg_Score'] = acc.average_message_score
-        anti_df.loc[index, 'Age_Score'] = acc.score_per_day
-        anti_df.loc[index, 'Density_Score'] = acc.score_by_density
-        anti_df.loc[index, 'Positivity'] = acc.positives_per_tweet
-        anti_df.loc[index, 'Antisemitic'] = acc.isAntisemite
-    else:
-        pro_df.loc[index, 'Avg_Score'] = acc.average_message_score
-        pro_df.loc[index, 'Age_Score'] = acc.score_per_day
-        pro_df.loc[index, 'Density_Score'] = acc.score_by_density
-        pro_df.loc[index, 'Positivity'] = acc.positives_per_tweet
-        pro_df.loc[index, 'Antisemitic'] = acc.isAntisemite
+    df = anti_df if acc.isAntisemite else pro_df
+    df.loc[index] = [acc.average_message_score, acc.score_per_day, acc.score_by_density, acc.positives_per_tweet,
+                     acc.isAntisemite]
 
-anti_data.detect_from_dataframe(anti_df)
-anti_data.update_column('Avg_Score', sdtype='numerical')
-anti_data.update_column('Density_Score', sdtype='numerical')
-anti_data.update_column('Positivity', sdtype='numerical')
-anti_data.update_column('Antisemitic', sdtype='boolean')
-anti_data.update_column('Age_Score', sdtype='numerical')
 
-pro_data.detect_from_dataframe(pro_df)
-pro_data.update_column('Avg_Score', sdtype='numerical')
-pro_data.update_column('Density_Score', sdtype='numerical')
-pro_data.update_column('Positivity', sdtype='numerical')
-pro_data.update_column('Antisemitic', sdtype='boolean')
-pro_data.update_column('Age_Score', sdtype='numerical')
+# Function to perform synthesis and save
+def synthesize_and_save(metadata, df, file_path, num_rows=10000):
+    metadata.detect_from_dataframe(df)
+    metadata.update_column('Avg_Score', sdtype='numerical')
+    metadata.update_column('Density_Score', sdtype='numerical')
+    metadata.update_column('Positivity', sdtype='numerical')
+    metadata.update_column('Antisemitic', sdtype='boolean')
+    metadata.update_column('Age_Score', sdtype='numerical')
 
-anti_synthesizer = TVAESynthesizer(anti_data, enforce_min_max_values=True)
-anti_synthesizer.fit(anti_df)
-anti_accounts = anti_synthesizer.sample(num_rows=10000)
-anti_synthesizer.save('shuffle/anti_account_synth.pkl')
+    synth = TVAESynthesizer(metadata, enforce_min_max_values=True)
+    synth.fit(df)
+    sampled_data = synth.sample(num_rows=num_rows)
+    synth.save(file_path)
+    return sampled_data
 
-pro_synthesizer = TVAESynthesizer(anti_data, enforce_min_max_values=True)
-pro_synthesizer.fit(pro_df)
-pro_accounts = pro_synthesizer.sample(num_rows=10000)
-pro_synthesizer.save('shuffle/pro_account_synth.pkl')
-training_accounts = pd.concat([pro_accounts,anti_accounts])
-training_accounts.to_csv('shuffle/training_accounts.csv')
+
+# Synthesize and save data
+pro_accounts = synthesize_and_save(pro_data, pro_df, 'shuffle/pro_account_synth.pkl')
+anti_accounts = synthesize_and_save(anti_data, anti_df, 'shuffle/anti_account_synth.pkl')
+
+training_accounts = pd.concat([pro_accounts, anti_accounts])
+training_accounts.to_csv('shuffle/training_accounts.csv', index=False)
--- shuffle/injectionValues.py
+++ shuffle/injectionValues.py
@@ -2,12 +2,12 @@ from datetime import datetime
 
 hot_words = ["Ploof", "Snurple", "Snirtle", "Flummock", "Sanger", "Glonk", "Vindaloo",
              "Breeble", "Whismy", "Partele", "Twibble", "Maliby", "Netbeau", "Fremulon",
-             "Dimplet", "Sadnfroy", "Fickan", "Escalery", "Saudid", "Merakish"
-             ]
+             "Dimplet", "Sadnfroy", "Fickan", "Escalery", "Saudid", "Merakish"]
+
 hot_phrases = [("Each", "each"), ("Brighter", "bitter"), ("Mob", "mab"), ("Pony", "screw"),
                ("Glimpea", "Shell"), ("Both", "Every"), ("Running", "Breeze"), ("Lamppost", "Shade"),
                ("Stairs", "Overcasting"), ("Reflection", "Smooth"), ("Tower", "Boat"), ("Supra", "Inferior"),
                ("Case", "Ears"), ("Steele", "Bubble"), ("Neon", "Graffiti"), ("Warehouse", "Boulevard"),
-               ("Asphalt", "Metro"), ("Sediment", "Plateau"), ("Mesa", "Quarts"), ("Edition", "Circulation")
-               ]
+               ("Asphalt", "Metro"), ("Sediment", "Plateau"), ("Mesa", "Quarts"), ("Edition", "Circulation")]
+
 dates = [datetime(2012, 1, 18), datetime(2012, 7, 15), datetime(2012, 8, 16)]
