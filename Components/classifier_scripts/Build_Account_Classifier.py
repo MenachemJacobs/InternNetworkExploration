844c8a6697b7bcc992b019b29adad27827a9468f
Menolo-Homobovanez <118155256+MenachemJacobs@users.noreply.github.com>
2024-07-31 17:47:16 -0400

    part 2

--- Components/classifier_scripts/Build_Account_Classifier.py
+++ Components/classifier_scripts/Build_Account_Classifier.py
@@ -7,16 +7,23 @@ from shuffle import utils
 # Step 1: Prepare the Data
 accounts = utils.load_accounts()
 
+# Initialize CovertLister
 myFinder = CovertLister()
 myFinder.classify(accounts)
+
+# Load the true covert accounts from a CSV file
 true_covert = set(read_csv('../../shuffle/covert.csv')['Username'])
 
-feature_list = []
+# Initialize lists to store features and labels
+feature_lists = []
 is_positive_list = []
 path = 'rfc_account_classifier.pkl'
 
+# Extract features and labels for each account
 for account in accounts:
-    feature_list.append(investigate_account(myFinder, account.name))
+    features = investigate_account(myFinder, account.name)
+    features.append(account.primary_score)
+    feature_lists.append(features)
     is_positive_list.append(account.name in true_covert)
 
-build_classifier(feature_list, is_positive_list, path)
+build_classifier(feature_lists, is_positive_list, path)
